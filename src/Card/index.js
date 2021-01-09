ac48e1838a21e7ba8cda1dc8f5a5e916f83a4652
olkhon <alexander.stapel@gmail.com>
2021-01-09 20:49:34 +0100

    add card component

---
+++ src/Card/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+
+export default function Card () {
+    return (
+        <div className="card">
+        <img src="https://picsum.photos/200/300" alt="" />
+        <br />
+        <h3>
+            default
+        </h3>
+        <br />
+        <p>
+        default
+        </p>
+      </div>
+    )
+}
